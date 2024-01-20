8baf18df1a4e93bf8703a81f5d1d935cc8b08114
ManuLazcano <manu.lazcano17@gmail.com>
2024-01-20 15:03:14 -0300

    Se creo los componentes para la estructura básica

---
+++ src/pages/App/index.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { Home } from '../Home'
+import { MyAccount } from '../MyAccount'
+import { MyOrder } from '../MyOrder'
+import { MyOrders } from '../MyOrders'
+import { NotFound } from '../NotFound'
+import { SingIn } from '../SingIn'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-100'>
+        <Home />
+        <MyAccount />
+        <MyOrder />
+        <MyOrders />
+        <NotFound />
+        <SingIn />
+      </div>
+    </>
+  )
+}
+
+export default App
