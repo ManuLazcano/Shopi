f965a65c727f8c626be064b69598515a07f15b89
ManuLazcano <manu.lazcano17@gmail.com>
2024-03-02 16:43:28 -0300

    Se agregó la funcionalidad para saber si el usuario tiene cuenta o no para mostrar datos y habilitar-desabilitar botones

--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -3,10 +3,15 @@ import { Layout } from '../../components/Layout';
 import { AuthContext } from '../../Contexts/AuthContext';
 
 function SingIn() {    
-  const userString = localStorage.getItem('user');
-  const userParse = JSON.parse(userString);
+  const { account } = React.useContext(AuthContext);
 
-  const { isAuth, setIsAuth } = React.useContext(AuthContext);
+  const accountInLocalStorage = localStorage.getItem('account');
+  const parsedAccount = JSON.parse(accountInLocalStorage);
+
+  // Has an account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
+  const noAccountInLocalState = account ? Object.keys(account).length === 0 : true;
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
 
   return (    
     <Layout>
@@ -14,20 +19,15 @@ function SingIn() {
           <h1 className="font-medium text-center mb-6">Sing in</h1>
           <form>
             <div className="flex flex-col">
-              <input className="border border-slate-300 hover:border-blue-300 focus:border-red-400 rounded-lg p-2 mb-3" type="email" placeholder="example@gmail.com" 
-                value={userParse?.email} 
-              />
-              <input className="border border-slate-300 hover:border-blue-300 focus:border-red-400 rounded-lg p-2 mb-3" type="password" placeholder="Password" 
-                value={userParse?.password} 
-              />
-              <button className="border h-10 rounded-md bg-blue-300 font-medium" type="submit"  
-                onClick={() => setIsAuth(true)}>
+              <input className="border border-slate-300 hover:border-blue-300 focus:border-red-400 rounded-lg p-2 mb-3" type="email" placeholder="example@gmail.com" value={parsedAccount.email}/>
+              <input className="border border-slate-300 hover:border-blue-300 focus:border-red-400 rounded-lg p-2 mb-3" type="password" placeholder="Password" value={parsedAccount.password}/>
+              <button className="border h-10 rounded-md bg-blue-300 font-medium" type="submit" disabled={!hasUserAnAccount}>
                 Log in
               </button>
             </div>
             <div className="my-3 text-center">
               <p className="text-gray-500 text-sm">Forgot my password</p>
-              <button className="w-full mt-5 border h-10 rounded-md bg-gray-200 font-medium" type="button">Sing up</button>
+              <button className="w-full mt-5 border h-10 rounded-md bg-gray-200 font-medium" type="button" disabled={hasUserAnAccount}>Sing up</button>
             </div>
           </form>
         </section>
