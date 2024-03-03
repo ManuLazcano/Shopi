96d73ff7985178314d84ed2963ae0fe063e9f292
ManuLazcano <manu.lazcano17@gmail.com>
2024-03-03 11:45:47 -0300

    Se agregó vista de creación de usuario y capturar info del form

--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Layout } from '../../components/Layout';
 import { AuthContext } from '../../Contexts/AuthContext';
+import { Link } from 'react-router-dom';
 
 function SingIn() {    
   const { account } = React.useContext(AuthContext);
   const [view , setView] = useState('user-info');
+  const form = useRef(null);
 
   const accountInLocalStorage = localStorage.getItem('account');
   const parsedAccount = JSON.parse(accountInLocalStorage);
@@ -13,6 +15,17 @@ function SingIn() {
   const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
   const noAccountInLocalState = account ? Object.keys(account).length === 0 : true;
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
+
+  const createAnAccount = () => {
+    const formData = new FormData(form.current);
+    const data = {
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password')
+    };
+
+    console.log(data);
+  }
 
   const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLogin();
   
@@ -43,13 +56,17 @@ function SingIn() {
     return (
       <section className="w-96">
           <h1 className="font-medium text-center mb-6">Sing up</h1>
-          <form>
+          <form ref={form}>
             <div className="flex flex-col">
-              <input className="border border-slate-300 hover:border-blue-300 focus:border-red-400 rounded-lg p-2 mb-3" type="text" placeholder="Name"/>
-              <input className="border border-slate-300 hover:border-blue-300 focus:border-red-400 rounded-lg p-2 mb-3" type="email" placeholder="example@gmail.com"/>
-              <input className="border border-slate-300 hover:border-blue-300 focus:border-red-400 rounded-lg p-2 mb-3" type="password" placeholder="Password"/>
-            <button className="border h-10 rounded-md bg-blue-300 font-medium" type="submit">Register</button>            
+              <input className="border border-slate-300 hover:border-blue-300 focus:border-red-400 rounded-lg p-2 mb-3" type="text" id="name" name="name" placeholder="Name"/>
+              <input className="border border-slate-300 hover:border-blue-300 focus:border-red-400 rounded-lg p-2 mb-3" type="email"id="email" name="email" placeholder="example@gmail.com"/>
+              <input className="border border-slate-300 hover:border-blue-300 focus:border-red-400 rounded-lg p-2 mb-3" type="password" id="password" name="password" placeholder="Password"/>
             </div>
+            <Link to="/">
+              <button className="border h-10 rounded-md bg-blue-300 font-medium" type="submit"
+                onClick={() => createAnAccount()}>
+                Register</button>            
+            </Link>            
           </form>
         </section>      
     );
